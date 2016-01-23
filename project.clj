6591008669ccaad40d1fc2e559d98d05e5539255
ewen <ewen.grosjean@free.fr>
2016-01-24 00:22:54 +0100

    update to clojure 1.8.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject ewen.replique/replique "0.0.1-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0-RC5"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.7.228"]
                  [ewen/hiccup "1.0.0"]
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]]
