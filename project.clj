f63291b0b4312ae704b9bdf3960ce3fd157c52ec
ewen <ewen.grosjean@free.fr>
2016-11-22 22:52:08 +0100

    clojure dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject replique/replique "0.0.1-SNAPSHOT"
-  :profiles {:provided {:dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                                       [org.clojure/clojurescript "1.9.229"]]}
-             :dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.9.229"]
+                                  [org.clojure/test.check "0.9.0"]]}}
   :source-paths ["src"]
   :eval-in-leiningen true)
