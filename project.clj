82012e777dcdacf5a3008f75b62a30b10cc9e9e8
ewen <ewen.grosjean@free.fr>
2016-11-24 12:24:52 +0100

    comment out the sourcemap namespace

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject replique/replique "0.0.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.9.229"]
-                                  [org.clojure/test.check "0.9.0"]]}}
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.8.40"]]}}
   :source-paths ["src"]
   :eval-in-leiningen true)
--- src/replique/sourcemap.clj
+++ src/replique/sourcemap.clj
@@ -1,25 +1,25 @@
 (ns replique.sourcemap
-  (:require [clojure.data.json :as json]
+  #_(:require [clojure.data.json :as json]
             [clojure.java.io :refer [as-file]])
   (:import [java.util Base64]
            [java.nio.charset StandardCharsets]
-           [java.nio.file Paths]))
+           #_[java.nio.file Paths]))
 
 ;;/*# sourceMappingURL=data:application/json;base64,ewoJInZlcnNpb24iOiAzLAoJImZpbGUiOiAidGVzdC5jc3MiLAoJInNvdXJjZXMiOiBbCgkJInRlc3Quc2NzcyIsCgkJInJlc2V0LnNjc3MiCgldLAoJInNvdXJjZXNDb250ZW50IjogW10sCgkibWFwcGluZ3MiOiAiQUFHRSxHQUFHLENBQUMsQ0FBQyxDQUFIO0VBQ0EsS0FBSyxFQUFFLElBQUssR0FEWDs7QUFLSCxDQUFDLENBQUMsQ0FBQyxDQUFEO0VBQ0EsS0FBSyxFQUFFLElBQUssR0FEWCIsCgkibmFtZXMiOiBbXQp9 */
 
-(defn str->path [s]
+#_(defn str->path [s]
   (if s
     (Paths/get s (make-array String 0))
     nil))
 
-(defn decode-base-64 [s-b64]
+#_(defn decode-base-64 [s-b64]
   (if (nil? s-b64)
     nil
     (-> (Base64/getDecoder)
         (.decode s-b64)
         (String.))))
 
-(defn encode-base-64 [s]
+(defn encode-base-64 [^String s]
   (if (nil? s)
     nil
     (let [b (.getBytes s StandardCharsets/UTF_8)]
@@ -27,45 +27,47 @@
           (.encode b)
           (String.)))))
 
-(def sourcemapping-regexp #"(?:\/\/[@#][ \t]+sourceMappingURL=([^\s'\"]+?)[ \t]*$)|(?:\/\*[@#][ \t]+sourceMappingURL=([^\*]+?)[ \t]*(?:\*\/)[ \t]*$)")
-(def data-uri-regexp #"^data:[^;]*(?:;base64)?,(.*)$")
+(comment
 
-(defn json-read-str [s]
-  (let [s (cond (nil? s) "null"
-                (= "" (.trim s)) "null"
-                :else s)]
-    (json/read-str s)))
+  (def sourcemapping-regexp #"(?:\/\/[@#][ \t]+sourceMappingURL=([^\s'\"]+?)[ \t]*$)|(?:\/\*[@#][ \t]+sourceMappingURL=([^\*]+?)[ \t]*(?:\*\/)[ \t]*$)")
+  (def data-uri-regexp #"^data:[^;]*(?:;base64)?,(.*)$")
 
-(def parse-data-uri
-  (fnil
-   (fn [s]
-     (let [matcher (re-matcher data-uri-regexp s)]
-       (second (re-find matcher))))
-   ""))
+  (defn json-read-str [s]
+    (let [s (cond (nil? s) "null"
+                  (= "" (.trim s)) "null"
+                  :else s)]
+      (json/read-str s)))
 
-(def parse-sourcemap
-  (fnil
-   (fn [s]
-     (let [matcher (re-matcher sourcemapping-regexp s)]
-       (-> (re-find matcher)
-           (nth 2)
-           parse-data-uri
-           decode-base-64
-           json-read-str)))
-   ""))
+  (def parse-data-uri
+    (fnil
+     (fn [s]
+       (let [matcher (re-matcher data-uri-regexp s)]
+         (second (re-find matcher))))
+     ""))
 
-(defn css-file->sourcemap [css-path]
-  (if (.exists (as-file css-path))
-    (-> (slurp css-path)
+  (def parse-sourcemap
+    (fnil
+     (fn [s]
+       (let [matcher (re-matcher sourcemapping-regexp s)]
+         (-> (re-find matcher)
+             (nth 2)
+             parse-data-uri
+             decode-base-64
+             json-read-str)))
+     ""))
+
+  (defn css-file->sourcemap [css-path]
+    (if (.exists (as-file css-path))
+      (-> (slurp css-path)
+          parse-sourcemap
+          (dissoc :sourcesContent :mappings))
+      nil))
+
+  (defn data->sourcemap [data]
+    (-> (parse-data-uri data)
+        decode-base-64
         parse-sourcemap
-        (dissoc :sourcesContent :mappings))
-    nil))
-
-(defn data->sourcemap [data]
-  (-> (parse-data-uri data)
-      decode-base-64
-      parse-sourcemap
-      (dissoc :sourcesContent :mappings)))
+        (dissoc :sourcesContent :mappings))))
 
 (comment
 
