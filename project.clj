fd0a47f130205e3835a6ffe9e95c68a361776421
ewen <ewen.grosjean@free.fr>
2017-06-29 23:03:51 +0200

    0.0.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject replique/replique "0.0.5-SNAPSHOT"
+(defproject replique/replique "0.0.5"
   :description "A development environment for Clojure and Clojurescript"
   :url "https://github.com/EwenG/replique"
   :license {:name "Eclipse Public License"
