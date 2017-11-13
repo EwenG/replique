c2add58a26ac18e4e89ea3a93bee0460aadd0f88
ewen <ewen.grosjean@free.fr>
2017-11-13 22:36:15 +0100

    add dev-resources/ which includes resources to test cljs upstream deps

--- test/replique/compliment/ns_mappings_cljs_test.cljs
+++ test/replique/compliment/ns_mappings_cljs_test.cljs
@@ -1,6 +1,8 @@
 (ns replique.compliment.ns-mappings-cljs-test
   (:require [clojure.string :as st :refer [split] :rename {split str-split}]
-            [replique.compliment.ns-mappings-cljs-test2 :refer [ff2]])
+            [replique.compliment.ns-mappings-cljs-test2 :refer [ff2]]
+            [replique.js-lib]
+            [replique.js-foreign-lib])
   (:import [goog.events EventType])
   (:require-macros [replique.compliment.ns-mappings-clj-test
                     :as cljs-ns-m :refer [my-macro mmmm] :rename {my-macro my-macro-2}]))
