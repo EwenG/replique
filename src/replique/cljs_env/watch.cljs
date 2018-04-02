e7169dc7394e6648946aef8e2bb4ad9baae4cd3d
ewen <ewen.grosjean@free.fr>
2018-04-02 23:48:03 +0200

    Add the possibility to browse a watched value

--- src/replique/cljs_env/watch.cljs
+++ src/replique/cljs_env/watch.cljs
@@ -1,6 +1,9 @@
 (ns replique.cljs-env.watch
   (:require [replique.cljs-env.repl :as repl]
-            [replique.cljs-env.elisp-printer :as elisp]))
+            [replique.cljs-env.elisp-printer :as elisp]
+            [replique.cljs-env.completion :as completion]
+            [cljs.reader :as reader]
+            [goog.object :as o]))
 
 (defonce watched-refs (atom {}))
 (defonce watched-refs-values (atom {}))
@@ -48,23 +51,110 @@
     (remove-watch watchable (keyword "replique.watch" (str buffer-id)))
     (swap! watched-refs dissoc buffer-id)))
 
-(defn update-replique-watch [process-id var-sym buffer-id print-length print-level]
+(defn browse-get [o k]
+  (cond (implements? ILookup o)
+        (get o k)
+        (object? o)
+        (o/get o k)
+        (array? o)
+        (aget o k)
+        (and (coll? o) (seqable? o))
+        (nth (seq o) k)
+        :else nil))
+
+(defn browse-get-in
+  ([m ks]
+   (reduce get m ks))
+  ([m ks not-found]
+   (loop [sentinel lookup-sentinel
+          m m
+          ks (seq ks)]
+     (if-not (nil? ks)
+       (let [m (get m (first ks) sentinel)]
+         (if (identical? sentinel m)
+           not-found
+           (recur sentinel m (next ks))))
+       m))))
+
+(defn parse-browse-path [browse-path]
+  (into '() (map reader/read-string) browse-path))
+
+(defn refresh-watch [process-id update? var-sym buffer-id
+                     print-length print-level browse-path]
   (let [watchable (get @watched-refs buffer-id)]
     (if (some? watchable)
-      (let [watchable-value (maybe-deref watchable)]
-        (swap! watched-refs-values assoc buffer-id watchable-value)
-        (binding [*print-length* print-length
-                  *print-level* print-level]
-          (pr-str watchable-value)))
+      (let [browse-path (parse-browse-path browse-path)]
+        (when update?
+          (swap! watched-refs-values assoc buffer-id (maybe-deref watchable)))
+        (let [watchable-value (get @watched-refs-values buffer-id)]
+          (binding [*print-length* print-length
+                    *print-level* print-level]
+            (pr-str (browse-get-in watchable-value browse-path)))))
       (let [watchable (maybe-nested-iref var-sym)]
         (if watchable
           (do
             (add-replique-watch process-id var-sym buffer-id)
-            (recur process-id var-sym buffer-id print-length print-level))
+            (recur process-id update? var-sym buffer-id
+                   print-length print-level browse-path))
+          (throw (js/Error. :replique-watch/undefined)))))))
+
+(declare serializable?)
+
+(defn serializable-map-entry? [e]
+  (and (serializable? (key e)) (serializable? (val e))))
+
+(defn serializable-object-entry? [v k o]
+  (and (serializable? k) (serializable? v)))
+
+(defn serializable? [x]
+  (cond (nil? x) true
+        (boolean? x) true
+        (number? x) true
+        (string? x) true
+        (symbol? x) true
+        (keyword? x) true
+        (map? x) (every? serializable-map-entry? x)
+        (object? x) (o/every serializable-object-entry? x)
+        (or (coll? x) (array? x)) (every? serializable? x)
+        :else false))
+
+(defn browse-candidates [process-id var-sym buffer-id prefix browse-path]
+  (let [watchable (get @watched-refs buffer-id)]
+    (if (some? watchable)
+      (let [watchable-value (maybe-deref watchable)
+            browse-path (parse-browse-path browse-path)
+            watchable-value (browse-get-in watchable-value browse-path)
+            prefix-tokens (completion/tokenize-prefix (str prefix))]
+        (-> (cond (or (map? watchable-value) (object? watchable-value))
+                 (doall
+                  (for [k (if (object? watchable-value)
+                            (o/getKeys watchable-value)
+                            (keys watchable-value))
+                        :when (and (completion/matches? (str k) prefix-tokens)
+                                   (serializable? k))]
+                    (binding [*print-length* nil
+                              *print-level* nil
+                              *print-meta* nil]
+                      (pr-str k))))
+                 (or (coll? watchable-value) (array? watchable-value))
+                 (doall
+                  (for [i (range (count watchable-value))
+                        :when (completion/matches? (str i) prefix-tokens)]
+                    (binding [*print-length* nil
+                              *print-level* nil
+                              *print-meta* nil]
+                      (pr-str i))))
+                 :else nil)
+            elisp/pr-str))
+      (let [watchable (maybe-nested-iref var-sym)]
+        (if watchable
+          (do
+            (add-replique-watch process-id var-sym buffer-id)
+            (recur process-id var-sym buffer-id prefix browse-path))
           (throw (js/Error. :replique-watch/undefined)))))))
 
 (comment
-  (def tt (atom [1 "e"]))
+  (def tt (atom {:e "e"}))
   (reset! tt '{:e "e" f [1 2 3 (ffff "e") 5]})
 
   (remove-watch tt (keyword "replique.watch" "1"))
--- src/replique/watch.clj
+++ src/replique/watch.clj
@@ -1,8 +1,10 @@
 (ns replique.watch
   (:require [replique.tooling-msg :as tooling-msg]
-            [replique.utils :as utils])
-  (:import [clojure.lang IRef IDeref IPending]
-           [java.util Map]))
+            [replique.utils :as utils]
+            [replique.completion :as completion]
+            [replique.elisp-printer :as elisp])
+  (:import [clojure.lang IRef IDeref IPending ILookup]
+           [java.util Map Collection]))
 
 (def ^:private cljs-repl-env (utils/dynaload 'replique.repl-cljs/repl-env))
 (def ^:private cljs-evaluate-form (utils/dynaload 'replique.repl-cljs/evaluate-form))
@@ -62,15 +64,115 @@
       (swap! watched-refs-values dissoc buffer-id)
       nil)))
 
-(defmethod tooling-msg/tooling-msg-handle [:replique/clj :update-watch]
-  [{:keys [buffer-id print-length print-level] :as msg}]
+;; Like clojure.core/get for ILookup and maps. Get the nth element for collections
+(defn browse-get [o k]
+  (cond (or (instance? ILookup o) (instance? Map o))
+        (get o k)
+        (and (or (coll? o) (instance? Collection o))
+             (seqable? o))
+        (nth (seq o) k)
+        :else nil))
+
+;; like clojure.core/get-in but uses browse-get instead of get
+(defn browse-get-in
+  ([m ks]
+   (reduce browse-get m ks))
+  ([m ks not-found]
+   (loop [sentinel (Object.)
+          m m
+          ks (seq ks)]
+     (if ks
+       (let [m (browse-get m (first ks) sentinel)]
+         (if (identical? sentinel m)
+           not-found
+           (recur sentinel m (next ks))))
+       m))))
+
+(defn parse-browse-path [browse-path]
+  (into '() (map read-string) browse-path))
+
+(defmethod tooling-msg/tooling-msg-handle [:replique/clj :refresh-watch]
+  [{:keys [buffer-id update? print-length print-level browse-path] :as msg}]
   (tooling-msg/with-tooling-response msg
     (let [ref (get @watched-refs buffer-id)
-          ref-value @ref]
-      (swap! watched-refs-values assoc buffer-id ref-value)
-      {:var-value (binding [*print-length* print-length
-                            *print-level* print-level]
-                    (pr-str ref-value))})))
+          browse-path (parse-browse-path browse-path)]
+      (when update?
+        (swap! watched-refs-values assoc buffer-id @ref))
+      (let [ref-value (get @watched-refs-values buffer-id)]
+        {:var-value (binding [*print-length* print-length
+                              *print-level* print-level]
+                      (pr-str (browse-get-in ref-value browse-path)))}))))
+
+(declare serializable?)
+
+(defn serializable-map-entry? [e]
+  (and (serializable? (key e)) (serializable? (val e))))
+
+(defn serializable? [x]
+  (cond (nil? x) true
+        (boolean? x) true
+        (number? x) true
+        (string? x) true
+        (symbol? x) true
+        (keyword? x) true
+        (or (map? x) (instance? Map x)) (every? serializable-map-entry? x)
+        (or (coll? x) (instance? Collection x)) (every? serializable? x)
+        :else false))
+
+(defmethod tooling-msg/tooling-msg-handle [:replique/clj :browse-candidates]
+  [{:keys [buffer-id prefix browse-path] :as msg}]
+  (tooling-msg/with-tooling-response msg
+    (let [ref (get @watched-refs buffer-id)
+          browse-path (parse-browse-path browse-path)
+          ref-value (browse-get-in @ref browse-path)
+          prefix-tokens (completion/tokenize-prefix (str prefix))]
+      ;; Only return candidates that can be read by the clojure/clojurescript reader.
+      ;; This allows keeping the browse path on the client side and thus makes serveral things
+      ;; easier to implement, like handling a browser refresh for example
+      {:candidates
+       (cond (or (map? ref-value) (instance? Map ref-value))
+             (doall
+              (for [k (keys ref-value)
+                    :when (and (completion/matches? (str k) prefix-tokens)
+                               (serializable? k))]
+                (binding [*print-length* nil
+                          *print-level* nil
+                          *print-meta* nil]
+                  (pr-str k))))
+             (or (coll? ref-value) (instance? Collection ref-value)
+                 (and (class ref-value) (.isArray (class ref-value))))
+             (doall
+              (for [i (range (count ref-value))
+                    :when (completion/matches? (str i) prefix-tokens)]
+                (binding [*print-length* nil
+                          *print-level* nil
+                          *print-meta* nil]
+                  (pr-str i))))
+             :else nil)})))
+
+(comment
+  (.isArray (class (int-array 2)))
+  (.isArray (class nil))
+  (get (int-array 2) 1)
+
+  (instance? java.util.Map (doto (java.util.HashMap.)
+                             (.put "e" 3)))
+  (map key (doto (java.util.HashMap.)
+                     (.put "e" 3)))
+  (coll? "ee")
+  (instance? java.util.Collection (doto (java.util.ArrayList.)
+                                    (.add 3)))
+  (nth (doto (java.util.ArrayList.)
+         (.add 3)) 0)
+  (get (doto (java.util.ArrayList.)
+         (.add 3)) 1)
+  
+  (= (doto (java.util.ArrayList.)
+       (.add 3)) [3])
+
+  (coll? #{"e"})
+  (seqable? "ee")
+  )
 
 (defmethod tooling-msg/tooling-msg-handle [:replique/cljs :add-watch]
   [{:keys [process-id var-sym buffer-id] :as msg}]
@@ -98,21 +200,44 @@
         {:error value}
         {}))))
 
-(defmethod tooling-msg/tooling-msg-handle [:replique/cljs :update-watch]
-  [{:keys [process-id var-sym buffer-id print-length print-level] :as msg}]
+(defn browse-path->js-array [browse-path]
+  `[~@(interpose (symbol ",") browse-path)])
+
+(defmethod tooling-msg/tooling-msg-handle [:replique/cljs :refresh-watch]
+  [{:keys [process-id update? var-sym buffer-id print-length print-level browse-path]
+    :as msg}]
   (tooling-msg/with-tooling-response msg
     (let [{:keys [status value stacktrace] :as ret}
           (@cljs-evaluate-form
            @@cljs-repl-env
-           (format "replique.cljs_env.watch.update_replique_watch(%s, %s, %s, %s, %s);"
-                   (pr-str process-id) (pr-str (@cljs-munged var-sym)) (pr-str buffer-id)
+           (format "replique.cljs_env.watch.refresh_watch(%s, %s, %s, %s, %s, %s, %s);"
+                   (pr-str process-id) (pr-str (boolean update?)) 
+                   (pr-str (@cljs-munged var-sym)) (pr-str buffer-id)
                    (if (nil? print-length) "null" (pr-str print-length))
-                   (if (nil? print-level) "null" (pr-str print-level)))
+                   (if (nil? print-level) "null" (pr-str print-level))
+                   (if (nil? browse-path) "null" (pr-str (browse-path->js-array browse-path))))
            :timeout-before-submitted 100)]
       (if-not (= status :success)
         {:error (or stacktrace value)
          :undefined (.contains ^String value (str :replique-watch/undefined))}
         {:var-value value}))))
+
+(defmethod tooling-msg/tooling-msg-handle [:replique/cljs :browse-candidates]
+  [{:keys [process-id var-sym buffer-id prefix browse-path] :as msg}]
+  (tooling-msg/with-tooling-response msg
+    (let [{:keys [status value stacktrace] :as ret}
+          (@cljs-evaluate-form
+           @@cljs-repl-env
+           (format "replique.cljs_env.watch.browse_candidates(%s, %s, %s, %s, %s);"
+                   (pr-str process-id) (pr-str (@cljs-munged var-sym)) (pr-str buffer-id)
+                   (pr-str prefix) (if (nil? browse-path)
+                                     "null"
+                                     (pr-str (browse-path->js-array browse-path))))
+           :timeout-before-submitted 100)]
+      (if-not (= status :success)
+        {:error (or stacktrace value)
+         :undefined (.contains ^String value (str :replique-watch/undefined))}
+        {:candidates (elisp/->ElispString value)}))))
 
 (comment
   (def tt (atom 2))
