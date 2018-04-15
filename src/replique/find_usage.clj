a44dd2a9bbb4b7c4d4d0108d3cb30ea94db372a9
ewen <ewen.grosjean@free.fr>
2018-04-15 23:10:49 +0200

    find-usage for classes/imports and namespaces

--- src/replique/find_usage.clj
+++ src/replique/find_usage.clj
@@ -1,6 +1,7 @@
 (ns replique.find-usage
   (:require [replique.environment :as env]
-            [replique.context :as context]))
+            [replique.context :as context])
+  (:import [replique.environment CljsCompilerEnv]))
 
 (defn var-symbols-in-namespaces-reducer
   [comp-env var-ns var-sym var m ns]
@@ -76,33 +77,86 @@
   (try (env/ns-resolve comp-env ns sym)
        (catch ClassNotFoundException e nil)))
 
+(defn ns-imports-symbols [comp-env ns sym]
+  (doall (for [[k v] (env/ns-imports comp-env ns)
+               :when (= v sym)]
+           k)))
+
+(defn symbols-in-namespace-imports [comp-env sym symbols ns]
+  (assoc! symbols (str ns) (ns-imports-symbols comp-env ns sym)))
+
+(defn symbols-in-namespaces-imports [comp-env sym]
+  (->> (env/all-ns comp-env)
+       (reduce (partial symbols-in-namespace-imports comp-env sym) (transient {}))
+       persistent!))
+
+(defn ns-aliases-symbols [comp-env ns target-ns]
+  (doall (for [[k v] (env/ns-aliases comp-env ns)
+               :when (= v target-ns)]
+           k)))
+
+(defn symbols-in-namespace-alias [comp-env target-ns symbols ns]
+  (assoc! symbols (str ns) (ns-aliases-symbols comp-env ns target-ns)))
+
+(defn symbols-in-namespaces-aliases [comp-env target-ns]
+  (->> (env/all-ns comp-env)
+       (reduce (partial symbols-in-namespace-alias comp-env target-ns) (transient {}))
+       persistent!))
+
 (defn symbols-in-namespaces
   [comp-env ns
    {:keys [at-local-binding-position? in-comment? in-string? locals] :as context}
    ^String prefix]
   (let [ns (or (and ns (env/find-ns comp-env (symbol ns)))
                (env/find-ns comp-env (env/default-ns comp-env)))]
-    (when (and prefix
-               (not at-local-binding-position?) (not in-comment?) (not in-string?))
+    (when (and prefix (not at-local-binding-position?) (not in-comment?) (not in-string?))
       (let [prefix (.replaceFirst prefix "^#_" "")
             keyword? (.startsWith prefix ":")]
-        (if keyword?
-          (symbols-in-namespaces-keyword comp-env ns context prefix)
-          (when (and (not (contains? locals prefix))
-                     (not (contains? (env/special-forms comp-env) prefix)))
-            (let [prefix-sym (symbol prefix)
-                  resolved (safe-ns-resolve comp-env ns prefix-sym)
-                  resolved (when (env/looks-like-var? comp-env resolved) resolved)
-                  m (env/meta comp-env resolved)
-                  var-ns (:ns m)
-                  var-sym (:name m)]
-              (when (and resolved var-ns var-sym)
-                {:symbols-in-namespaces
-                 (var-symbols-in-namespaces
-                  comp-env var-ns var-sym resolved
-                  (env/all-ns comp-env))
-                 :find-usage-type :var
-                 :var (symbol (str var-ns) (str var-sym))}))))))))
+        (cond keyword?
+              (symbols-in-namespaces-keyword comp-env ns context prefix)
+              (contains? (env/special-forms comp-env) prefix)
+              {:find-usage-type :special-form
+               :special-form prefix}
+              :else
+              (when (not (contains? locals prefix))
+                (let [prefix-sym (symbol prefix)
+                      resolved (safe-ns-resolve comp-env ns prefix-sym)
+                      resolved-ns (when (nil? resolved) (env/find-ns comp-env prefix-sym))
+                      resolved-ns (if (and (nil? resolved) (nil? resolved-ns))
+                                    (get (env/ns-aliases comp-env ns) prefix-sym)
+                                    (or resolved resolved-ns))]
+                  (cond (env/looks-like-var? comp-env resolved)
+                        (let [m (env/meta comp-env resolved)
+                              var-ns (:ns m)
+                              var-sym (:name m)]
+                          (when (and resolved var-ns var-sym)
+                            {:symbols-in-namespaces
+                             (var-symbols-in-namespaces
+                              comp-env var-ns var-sym resolved
+                              (env/all-ns comp-env))
+                             :find-usage-type :var
+                             :var (symbol (str var-ns) (str var-sym))}))
+                        ;; A JVM class or 
+                        (class? resolved)
+                        (let [class-symbol (symbol (.getName ^Class resolved))]
+                          {:symbols-in-namespaces (symbols-in-namespaces-imports
+                                                   comp-env class-symbol)
+                           :find-usage-type :class
+                           :class class-symbol})
+                        ;; a cljs import
+                        ;; only imported symbols are resolved, not the fully qualified symbol
+                        ;; of a js-dependency-index provide. This is different from the way it
+                        ;; works for jvm classes
+                        (and (instance? CljsCompilerEnv comp-env) (symbol? resolved))
+                        {:symbols-in-namespaces (symbols-in-namespaces-imports
+                                                 comp-env resolved)
+                         :find-usage-type :class
+                         :class resolved}
+                        (some? resolved-ns)
+                        {:symbols-in-namespaces (symbols-in-namespaces-aliases
+                                                 comp-env resolved-ns)
+                         :find-usage-type :namespace
+                         :namespace (env/ns-name resolved-ns)}))))))))
 
 (comment
   (let [v #'replique.watch/browse-get]
