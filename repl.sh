0c7095d3adcb7f9e45a4813e3511a94702e1a84b
ewen <ewen.grosjean@free.fr>
2018-07-26 00:00:08 +0200

    repl.sh command line update

--- repl.sh
+++ repl.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-clojure -A:dev -Sdeps "{:deps {org.clojure/clojurescript {:mvn/version \"1.9.473\"} org.clojure/clojure {:mvn/version \"1.10.0-alpha6\"}}}"
-
-## {org.clojure/clojurescript {:mvn/version "1.9.946"}}
-## {org.clojure/clojurescript {:mvn/version "1.10.238"}}
-## {org.clojure/clojurescript {:mvn/version "1.10.339"}}
+clojure -A:dev 
