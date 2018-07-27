63fdd5797cd58a1f029fb9fe48b27a614e711cb4
ewen <ewen.grosjean@free.fr>
2018-07-27 23:49:49 +0200

    build script

--- src-build/replique/build.clj
+++ src-build/replique/build.clj
@@ -2,18 +2,31 @@
   (:require [replique.utils :as utils]
             [badigeon.clean :as clean]
             [badigeon.jar :as jar]
+            [badigeon.install :as install]
             [badigeon.sign :as sign]
             [badigeon.prompt :as prompt]
             [badigeon.deploy :as deploy]
             [clojure.tools.deps.alpha.util.maven :as maven]))
 
-(defn -main []
+(defn make-jar []
   (clean/clean "target")
-  (let [jar-path (jar/jar 'replique/replique {:mvn/version utils/version})
+  (jar/jar 'replique/replique {:mvn/version utils/version}))
+
+(defn deploy-local []
+  (let [jar-path (make-jar)]
+    (install/install 'replique/replique {:mvn/version utils/version}
+                     jar-path "pom.xml")))
+
+(defn -main []
+  (let [jar-path (make-jar)
         artifacts (sign/sign [{:file-path jar-path} {:file-path "pom.xml"}])
         password (prompt/prompt-password "Password:")]
-    (badigeon.deploy/deploy 'replique/replique "0.0.15"
+    (badigeon.deploy/deploy 'replique/replique utils/version artifacts
                             {:id "clojars"
                              :url "https://repo.clojars.org/"}
                             {:credentials {:username "ewen"
                                            :password password}})))
+
+(comment
+  (deploy-local)
+  )
