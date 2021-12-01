38fbc2f824a7bb95d491f845cfdda8da61b089c5
ewen <ewen.grosjean@free.fr>
2021-12-01 15:18:02 +0100

    cljs test file

--- test/replique/compliment/ns_mappings_cljs_test2.cljs
+++ test/replique/compliment/ns_mappings_cljs_test2.cljs
@@ -1,3 +1,10 @@
-(ns replique.compliment.ns-mappings-cljs-test2)
+(ns replique.compliment.ns-mappings-cljs-test2
+  (:require [goog.object]))
 
 (def ff2 "ff2")
+
+(goog.object/get #js {:e "e"} "e")
+
+(comment
+  (goog.object/get #js {:e "e"} :e)
+  )
